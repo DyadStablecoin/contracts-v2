2c36f48fcf27e92f35d9900af2d6e9b1774283d0
shafu <selfouly@gmail.com>
2023-01-11 18:50:41 +0100

    init Manager contract

---
+++ src/core/Manager.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity = 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract Manager is ERC721Enumerable {}
