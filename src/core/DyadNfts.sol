e8f91cab9a069e8f20b274f863d989d8f392a2c0
shafu <selfouly@gmail.com>
2023-01-11 19:14:37 +0100

    DeployBase

--- src/core/DyadNfts.sol
+++ src/core/DyadNfts.sol
@@ -3,12 +3,24 @@ pragma solidity = 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
+import {Dyad} from "./Dyad.sol";
+
 contract DyadNfts is ERC721Enumerable {
-  constructor() ERC721("DYAD NFT", "dNFT") {}
+  uint public immutable MAX_SUPPLY;                
 
   mapping(uint256 => DyadNftData) public getDyadNftData;
 
+  Dyad public dyad;
+
   struct DyadNftData {
     uint xp;
+  }
+
+  constructor(
+    address _dyad,
+    uint    _maxSupply
+  ) ERC721("Dyad NFT", "dNFT") {
+    dyad       = Dyad(_dyad);
+    MAX_SUPPLY = _maxSupply;
   }
 }
