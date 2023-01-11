9fcf355f29625783b268eb0750461d2432f81fa9
shafu <selfouly@gmail.com>
2023-01-11 19:28:59 +0100

    first tests

--- script/deploy/DeployBase.s.sol
+++ script/deploy/DeployBase.s.sol
@@ -5,17 +5,16 @@ import "forge-std/Script.sol";
 import {Dyad} from "../../src/core/Dyad.sol";
 import {DyadNfts} from "../../src/core/DyadNfts.sol";
 
-contract Deployment is Script {
-
-  function deploy(uint _maxSupply) public {
+contract DeployBase is Script {
+  function deploy() public returns (address, address) {
     vm.startBroadcast();
 
     Dyad     dyad     = new Dyad();
-    DyadNfts dyadNfts = new DyadNfts(address(dyad), _maxSupply);
+    DyadNfts dyadNfts = new DyadNfts(address(dyad));
 
     dyad.transferOwnership(address(dyadNfts));
 
     vm.stopBroadcast();
+    return (address(dyadNfts), address(dyad));
   }
-
 }
