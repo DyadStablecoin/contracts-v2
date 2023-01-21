4277c2803b3b8b2194d1f767517cea6cf51cd0ae
shafu.eth <selfouly@gmail.com>
2023-01-21 04:42:10 +0100

    auto claim

--- src/staking/AutoClaim.sol
+++ src/staking/AutoClaim.sol
@@ -6,6 +6,7 @@ import {Owned} from "@solmate/src/auth/Owned.sol";
 
 contract AutoClaim is Owned {
 
+  uint constant MAX_STAKER = 100;
   uint public masterDNft;
   DNft public dNft;
   mapping(uint => address) public owners;
@@ -20,7 +21,13 @@ contract AutoClaim is Owned {
     masterDNft = _masterDNft;
   }
 
-  function stake() external {}
+  function setMasterDNft(uint _masterDNft) external onlyOwner {
+    masterDNft = _masterDNft;
+  }
+
+  function stake() external {
+    require(dNft.balanceOf(address(this)) < MAX_STAKER);
+  }
 
   function unstake(uint id) external onlyStakeOwner(id) {}
 
@@ -29,6 +36,9 @@ contract AutoClaim is Owned {
     for (uint i = 0; i < numberOfStakedNfts; i++) { // iterate over all staked dNfts
       uint id   = dNft.tokenOfOwnerByIndex(address(this), i);
       int share = dNft.claim(id);
+      if (share > 0) {
+        dNft.move(id, masterDNft, 100);
+      }
     }
   }
 }
