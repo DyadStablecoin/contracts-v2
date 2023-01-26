415a652ccacbf62a8d716f46c489038bbbc8c859
shafu.eth <selfouly@gmail.com>
2023-01-26 12:50:57 +0100

    more deposit tests

--- test/DNft.t.sol
+++ test/DNft.t.sol
@@ -150,6 +150,16 @@ contract DNftsTest is BaseTest {
     dNft.withdraw(id, address(this), 2000*1e18);
     dNft.deposit(id, 2000*1e18);
   }
+  function testCannotDepositMissingPermission() public {
+    vm.expectRevert(abi.encodeWithSelector(IDNft.MissingPermission.selector, 0, Permission.DEPOSIT));
+    dNft.deposit(0, 2000*1e18);
+  }
+  function testCannotDepositNftIsInactive() public {
+    uint id = dNft.mint{value: 50 ether}(address(this));
+    dNft.deactivate(id);
+    vm.expectRevert(abi.encodeWithSelector(IDNft.IsInactive.selector, id));
+    dNft.deposit(id, 2000*1e18);
+  }
 
   // -------------------- redeem --------------------
   function testRedeem() public {
